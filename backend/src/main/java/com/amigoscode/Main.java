96a0c8997e03e09196d69a868ac51b715a595de2
StanimalTheMan <sjc591@nyu.edu>
2023-09-29 14:05:24 -0400

    spring boot backend

---
+++ backend/src/main/java/com/amigoscode/Main.java
@@ -0,0 +1,61 @@
+package com.amigoscode;
+
+import com.amigoscode.customer.Customer;
+import com.amigoscode.customer.CustomerRepository;
+import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Random;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+
+//        ConfigurableApplicationContext applicationContext = SpringApplication.run(Main.class, args);
+
+//         printBeans(applicationContext);
+
+    }
+
+    @Bean
+    CommandLineRunner runner(CustomerRepository customerRepository) {
+        return args -> {
+            var faker = new Faker();
+            Random random = new Random();
+            Name name = faker.name();
+            String firstName = name.firstName();
+            String lastName = name.lastName();
+            Customer customer = new Customer(
+                    firstName + " " + lastName,
+                    firstName.toLowerCase() + "." + lastName.toLowerCase() + "@amigoscode.com",
+                    random.nextInt(16, 99)
+            );
+
+            customerRepository.save(customer);
+        };
+    }
+
+//    @Bean("foo")
+//    public Foo getFoo() {
+//
+//        return new Foo("bar");
+//    }
+//
+//    record Foo(String name){}
+//
+//    private static void printBeans(ConfigurableApplicationContext ctx) {
+//        String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
+//        for (String beanDefinitionName : beanDefinitionNames) {
+//            System.out.println(beanDefinitionName);
+//        }
+//    }
+
+}

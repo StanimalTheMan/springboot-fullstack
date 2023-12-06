e60bf09cd1fac6c540cf032c34100fdafadcd77a
StanimalTheMan <sjc591@nyu.edu>
2023-12-05 22:19:11 -0500

    New React routes

--- frontend/react/src/Customer.jsx
+++ frontend/react/src/Customer.jsx
@@ -12,7 +12,7 @@ import CreateCustomerDrawer from "./components/customer/CreateCustomerDrawer.jsx
 import {errorNotification} from "./services/notification.js";
 
 
-const App = () => {
+const Customer = () => {
 
     const [customers, setCustomers] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -93,4 +93,4 @@ const App = () => {
     )
 }
 
-export default App;
+export default Customer;

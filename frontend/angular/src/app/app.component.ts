f0421bf2d4766d30813c60d575917aa7fe3eff5a
StanimalTheMan <sjc591@nyu.edu>
2023-12-30 22:46:57 -0500

    basics of angular

---
+++ frontend/angular/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular';
+  clickCount = 0;
+  lastCreatedElement = '';
+
+  handleChildClick() {
+    this.clickCount++;
+  }
+
+  displayLastCreatedElement(element: string) {
+    this.lastCreatedElement = element;
+  }
+}
---
+++ frontend/angular/src/app/my-first-component/my-first.component.ts
@@ -0,0 +1,10 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-my-first-component',
+  templateUrl: './my-first.component.html',
+  styleUrls: ['./my-first.component.scss']
+})
+export class MyFirstComponent {
+
+}

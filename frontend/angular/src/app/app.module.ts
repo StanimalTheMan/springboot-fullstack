2ced59131fad20b511ccff532905441572c18fa7
StanimalTheMan <sjc591@nyu.edu>
2024-01-19 20:33:36 -0500

    Cleanup dummy angular code, create Customer component

--- frontend/angular/src/app/app.module.ts
+++ frontend/angular/src/app/app.module.ts
@@ -1,30 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MyFirstComponent } from './my-first-component/my-first.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
-import {MyCalculatorService} from "./services/my-calculator.service";
-import { Page1Component } from './pages/page1/page1.component';
-import { Page2Component } from './pages/page2/page2.component';
-import { Page3Component } from './pages/page3/page3.component';
+import {InputTextModule} from "primeng/inputtext";
+import { CustomerComponent } from './components/customer/customer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyFirstComponent,
-    Page1Component,
-    Page2Component,
-    Page3Component
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    InputTextModule
   ],
   providers: [
-    MyCalculatorService
   ],
   bootstrap: [AppComponent]
 })

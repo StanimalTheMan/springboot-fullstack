fa231b57b74f9667ffdf31b4936e4db99f894ce5
StanimalTheMan <sjc591@nyu.edu>
2024-03-20 16:23:21 -0400

    Implement success login

--- frontend/angular/src/app/components/login/login.component.ts
+++ frontend/angular/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {AuthenticationRequest} from "../../models/authentication-request";
 import {AuthenticationService} from "../../services/authentication/authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -12,14 +13,16 @@ export class LoginComponent {
   errorMsg = '';
 
   constructor(
-    private authenticationService: AuthenticationService
+    private authenticationService: AuthenticationService,
+    private router: Router
   ) {}
   login() {
     this.errorMsg = '';
     this.authenticationService.login(this.authenticationRequest)
       .subscribe({
         next: (authenticationResponse) => {
-          console.log(authenticationResponse);
+          localStorage.setItem('user', JSON.stringify(authenticationResponse));
+          this.router.navigate(['customers']);
         },
         error: (err) => {
           this.errorMsg = 'Login and / or password is incorrect';

a9f2e38b327fb3c52084f6f7146131512f49cae5
StanimalTheMan <sjc591@nyu.edu>
2024-03-21 09:59:08 -0400

    Add Authorization Header to the Request to Display all Customers

--- frontend/angular/src/app/services/customer/customer.service.ts
+++ frontend/angular/src/app/services/customer/customer.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {CustomerDTO} from "../../models/customer-dto";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  findAll(): Observable<CustomerDTO[]> {
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzY29wZXMiOlsiUk9MRV9VU0VSIl0sInN1YiI6Im1pc3R5Lm1hcnZpbkBhbWlnb3Njb2RlLmNvbSIsImlzcyI6Imh0dHBzOi8vc3RhbmNob2kubmV0bGlmeS5hcHAvIiwiaWF0IjoxNzEwOTY4NTE4LCJleHAiOjE3MTIyNjQ1MTh9.xUzZ_HxG954uy_4B-g8-L5zcadA2T3ZY9AgJTPsZank');
+    return this.http.get<CustomerDTO[]>(
+      'http://localhost:8080/api/v1/customers',
+      {
+        headers
+      }
+    );
+  }
 }

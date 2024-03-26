86fc8aa67314d152121a9487839bd61d9d542f4a
StanimalTheMan <sjc591@nyu.edu>
2024-03-26 17:05:18 -0400

    Implement the Register UI

--- frontend/angular/src/app/components/register/register.component.ts
+++ frontend/angular/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  errorMsg = '';
+  constructor(
+    private router: Router
+  ) {
+  }
 
+  login() {
+    this.router.navigate(['login'])
+  }
 }
